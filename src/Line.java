186fb144d21661b35cdfce9fe5e598990ddfe7c7
Nick <62675916+MrSnowZ@users.noreply.github.com>
2022-09-08 09:27:08 -0600

    take trevors code

--- src/Line.java
+++ src/Line.java
@@ -1,50 +1,62 @@
-/*
-
-2. Make a default constructor and an overloaded constructor.
-
-3. Write a toString for your line class.
-
-4. Write a main to test your line class. All functions should be tested.
-
-5. Write a function to test if a point is on the line, this should take in a 'x' and a 'y' and return true or false.
-
-6. Write all required setters and getters.
-
-7. Write an intersection method to find where two lines intersect. This is a void method that prints to the screen. */
-
-//import java.util.Scanner;
-// Scanner keyboard = new Scanner(System.in);
+public class Line {
+    double slope;
+    double intercept;
 
 
-public class Line
-{
-    // y=mx+b
-    double slope; // m
-    double yInt; // y intercept
-
-    public Line() //setter
+    public Line()
     {
-    //
-    slope = 2;
-    yInt = 1;
+        slope = 1;
+        intercept = 0;
     }
-    public Line(double slope, double b)
+    public Line(double slope, double intercept)
     {
         this.slope = slope;
-        yInt = b;
-    } //getter
+        this.intercept = intercept;
+    }
+    public void setSlope(double newSlope)
+    {
+        slope = newSlope;
+    }
+    public double getSlope()
+    {
+        return slope;
+    }
+    public void setIntercept(double newIntercept)
+    {
+        intercept = newIntercept;
+    }
+    public double getIntercept()
+    {
+        return intercept;
+    }
+    double x;
+    double y;
 
+    public void onLine()
+    {
+        x = 0;
+        y = 0;
+        
+    }
+    public void setx(double newx)
+    {
+        x = newx;
+    }
+    public double getx()
+    {
+        return x;
+    }
+    public void sety(double newy)
+    {
+        y = newy;
+    }
+    public double gety()
+    {
+        return y;
+    }
     public boolean onLine(double x, double y)
     {
-        if ()
-        return false;
+        double lineY = slope*x+intercept;
+        
     }
-
-    public void intersect(Line other)
-    {
-        //
-    }
-
-
 }
-
